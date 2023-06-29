d7cea141792dc382ac4596a9f03ddf084e30ec19
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2023-06-29 23:54:22 +0100

    feat: refresh token and access token strategies

---
+++ src/auth/strategy/access-token.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+// import { JwtPayload } from '../types';
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get<string>('AT_SECRET'),
+    });
+  }
+
+  validate(payload: any) {
+    return payload;
+  }
+}
---
+++ src/auth/strategy/refresh-token.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+// import { JwtPayload } from '../types';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get<string>('RF_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+
+  validate(req: Request, payload: any) {
+    const refreshToken = req.get('Authorization').replace('Bearer ', '').trim();
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
